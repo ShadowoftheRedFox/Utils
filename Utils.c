0516d4cb5c74fd4ea86f1179b76209a8cf8d76d8
ShadowoftheRedFox <leon.merin@gmail.com>
2023-10-18 12:02:36 +0200

    bug correction, features added
    error are shown between [], added warn and error with special property in code error, added a scanf_s if it's not defined

--- Utils.c
+++ Utils.c
@@ -21,8 +21,8 @@ int randInterval(const int min, const int max) {
 void qsrand() {
 #ifndef RANDOM_IS_SEEDED
 #define RANDOM_IS_SEEDED
-#endif
     srand(time(NULL));
+#endif
 }
 
 void empty_stdin() {
@@ -33,39 +33,53 @@ void showError(Error_Code code, Bool fatal, char* message) {
     switch (code) {
     case CODE_OK:
         if (message != NULL) {
-            printf("OK | %s\n", message);
+            printf("[OK] %s\n", message);
         } else {
-            printf("OK\n");
+            printf("[OK]\n");
         }
     case CODE_MALLOC_FAILED:
         if (message != NULL) {
-            printf("Malloc failed | %s\n", message);
+            printf("[Malloc failed] %s\n", message);
         } else {
-            printf("Malloc failed\n");
+            printf("[Malloc failed]\n");
         }
     case CODE_ARG_NULL:
         if (message != NULL) {
-            printf("Argument null | %s\n", message);
+            printf("[Argument null] %s\n", message);
         } else {
-            printf("Argument null\n");
+            printf("[Argument null]\n");
         }
     case CODE_ARG_INVALID:
         if (message != NULL) {
-            printf("Argument invalid | %s\n", message);
+            printf("[Argument invalid] %s\n", message);
         } else {
-            printf("Argument invalid\n");
+            printf("[Argument invalid]\n");
         }
     case CODE_THRESHOLD_SURPASSED:
         if (message != NULL) {
-            printf("Threshold surpassed | %s\n", message);
+            printf("[Threshold surpassed] %s\n", message);
         } else {
-            printf("Threshold surpassed\n");
+            printf("[Threshold surpassed]\n");
         }
+    case CODE_WARNING:
+        if (message != NULL) {
+            printf("[WARN] %s\n", message);
+        } else {
+            printf("[WARN]\n");
+        }
+        fatal = false;
+    case CODE_ERROR:
+        if (message != NULL) {
+            printf("[ERROR] %s\n", message);
+        } else {
+            printf("[ERROR]\n");
+        }
+        fatal = true;
     default:
         if (message != NULL) {
-            printf("Unknown code | %s\n", message);
+            printf("[Unknown code] %s\n", message);
         } else {
-            printf("Unknown code\n");
+            printf("[Unknown code]\n");
         }
         break;
     }
@@ -74,3 +88,22 @@ void showError(Error_Code code, Bool fatal, char* message) {
         exit(1);
     }
 }
+
+#ifndef scanf_s
+int scanf_s(const void* value, char* format, char* message, Bool(*conditions(const void* value))) {
+    if (value == NULL | conditions == NULL | format == NULL) {
+        showError(CODE_ARG_NULL, false, "scanf_s salue and/or conditions and/or format is NULL");
+        return -1;
+    }
+
+    int correct = 0;
+    do {
+        if (message != NULL) {
+            printf("%s", message);
+            correct = scanf(format, value);
+            empty_stdin();
+        }
+    } while (correct != 1 | !conditions(value));
+    return correct;
+}
+#endif
--- Utils.h
+++ Utils.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_UTILS_HANDOUT
 #define HEADER_UTILS_HANDOUT
+#include <stdio.h>
 
 typedef enum Bool {
     false, true
@@ -10,7 +11,11 @@ typedef enum Error_Code {
     CODE_MALLOC_FAILED,
     CODE_ARG_NULL,
     CODE_ARG_INVALID,
-    CODE_THRESHOLD_SURPASSED
+    CODE_THRESHOLD_SURPASSED,
+    // can not be fatal
+    CODE_WARNING,
+    // always fatal
+    CODE_ERROR
 } Error_Code;
 
 // renvoie des entiers inclus dans [min, max]
@@ -25,5 +30,12 @@ void empty_stdin();
 
 // show an error in the console
 void showError(Error_Code code, Bool fatal, char* message);
+
+#ifndef scanf_s
+// define a secure scanf (if you ask for an int, do not flood the console as en exemple)
+int scanf_s(const void* value, char* format, char* message, Bool(*conditions(const void* value))); //TODO finish
+#endif
+
+//TODO give description for everything
 
 #endif
